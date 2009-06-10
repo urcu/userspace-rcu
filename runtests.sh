b4fde1d45c43e18b0589a983509bea738c54cbce
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2009-06-10 18:44:58 -0400

    fix runtests.sh
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,7 @@
 
 #add urcu mb
 
-for a in test_urcu test_urcu_mb test_qsbr test_rwlock test_perthreadlock
+for a in test_urcu test_urcu_mb test_qsbr test_rwlock test_perthreadlock \
 			test_mutex; do
 	echo Executing $a
 	./${a} $*
