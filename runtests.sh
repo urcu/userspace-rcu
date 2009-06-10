bdbcf417e553b2eec9bb9a2c5a646dab77c8530c
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2009-06-10 18:43:37 -0400

    runtests include mutex test
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,8 @@
 
 #add urcu mb
 
-for a in test_urcu test_urcu_mb test_qsbr test_rwlock test_perthreadlock; do
+for a in test_urcu test_urcu_mb test_qsbr test_rwlock test_perthreadlock
+			test_mutex; do
 	echo Executing $a
 	./${a} $*
 done
