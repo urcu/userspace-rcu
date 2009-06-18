3e1c1b9a2a5c024de4a01623b3dc343484c7120c
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2009-06-18 12:42:06 -0400

    update runtests.sh
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for a in test_urcu test_urcu_mb test_qsbr test_rwlock test_perthreadlock \
+for a in test_urcu_gc test_urcu_gc_mb test_urcu test_urcu_mb \
+			test_qsbr test_qsbr_gc test_rwlock test_perthreadlock \
 			test_mutex; do
 	echo "./${a} $*" | tee -a runall.detail.log
 	/usr/bin/time --append --output runall.detail.log ./${a} $*
