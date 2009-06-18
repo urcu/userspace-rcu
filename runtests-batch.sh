657a89f1165f9294695173e0de031e1150768edd
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2009-06-18 12:33:47 -0400

    Make batch test shorter
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- runtests-batch.sh
+++ runtests-batch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for a in test_urcu_gc test_urcu_gc_mb test_qsbr_gc; do
+#for a in test_urcu_gc test_urcu_gc_mb test_qsbr_gc; do
+for a in test_urcu_gc; do
 	echo "./${a} $*" | tee -a runall.detail.log
 	/usr/bin/time --append --output runall.detail.log ./${a} $*
 done
