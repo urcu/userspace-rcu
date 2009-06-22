a74e263cde8d0d276c6ffffecee61c64e612eed0
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2009-06-22 10:55:39 -0400

    Update default batch size to 32768, after removal of mutex
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- runall.sh
+++ runall.sh
@@ -43,8 +43,8 @@ for BATCH_SIZE in ${BATCH_ARRAY}; do
 	./runtests-batch.sh ${NR_READERS} ${NR_WRITERS} ${DURATION} -d 0 -b ${BATCH_SIZE} ${EXTRA_OPTS} | tee -a batch-rcu.log
 done
 
-#setting gc each 4096. ** UPDATE FOR YOUR ARCHITECTURE BASED ON TEST ABOVE **
-EXTRA_OPTS+="-b 4096"
+#setting gc each 32768. ** UPDATE FOR YOUR ARCHITECTURE BASED ON TEST ABOVE **
+EXTRA_OPTS+="-b 32768"
 
 echo Executing update fraction test
 
