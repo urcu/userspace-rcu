e72f49371b233637eaf143265a73c7322d0710fd
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-09-23 03:09:59 -0400

    ppc atomic: Fix asm format.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- arch_atomic_ppc.h
+++ arch_atomic_ppc.h
@@ -120,7 +120,7 @@ unsigned long _atomic_cmpxchg(void *addr, unsigned long old,
 			"bne- 1b\n"	 	/* retry if lost reservation */
 			"isync\n"
 		"2:\n"
-				: "=&r"(old_val),
+				: "=&r"(old_val)
 				: "r"(addr), "r"((unsigned int)_new),
 				  "r"((unsigned int)old)
 				: "memory", "cc");
