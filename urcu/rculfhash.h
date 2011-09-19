cf77d1fac55a2b5c711d26b6fff22c68cf52dc72
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-09-19 16:12:43 -0400

    rculfhash: comment file inclusion order
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- urcu/rculfhash.h
+++ urcu/rculfhash.h
@@ -21,6 +21,8 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Include this file _after_ including your URCU flavor.
  */
 
 #include <stdint.h>
