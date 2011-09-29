2ef1bfb2cb9abb657e077814e272801e197a3c47
Lai Jiangshan <laijs@cn.fujitsu.com>
2011-09-29 13:47:13 -0400

    urcu,defer_rcu: Make defer_rcu encoding more compact for marker
    When the function changes (and the function is aligned), and only the
    data is the marker, we can get away with using only 2 pointers rather
    than 3.
    
    [ Edit by Mathieu Desnoyers: patch cleanup, changelog updates ]
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- urcu-defer-impl.h
+++ urcu-defer-impl.h
@@ -61,7 +61,9 @@
  * Assumes that (void *)-2L is not used often. Used to encode non-aligned
  * functions and non-aligned data using extra space.
  * We encode the (void *)-2L fct as: -2L, fct, data.
- * We encode the (void *)-2L data as: -2L, fct, data.
+ * We encode the (void *)-2L data as either:
+ *   fct | DQ_FCT_BIT, data (if fct is aligned), or
+ *   -2L, fct, data (if fct is not aligned).
  * Here, DQ_FCT_MARK == ~DQ_FCT_BIT. Required for the test order.
  */
 #define DQ_FCT_BIT		(1 << 0)
@@ -322,31 +324,33 @@ void _defer_rcu(void (*fct)(void *p), void *p)
 		assert(head - CMM_LOAD_SHARED(defer_queue.tail) == 0);
 	}
 
-	if (unlikely(defer_queue.last_fct_in != fct)) {
+	/*
+	 * Encode:
+	 * if the function is not changed and the data is aligned and it is
+	 * not the marker:
+	 *	store the data
+	 * otherwise if the function is aligned and its not the marker:
+	 *	store the function with DQ_FCT_BIT
+	 *	store the data
+	 * otherwise:
+	 *	store the marker (DQ_FCT_MARK)
+	 *	store the function
+	 *	store the data
+	 *
+	 * Decode: see the comments before 'struct defer_queue'
+	 *         or the code in rcu_defer_barrier_queue().
+	 */
+	if (unlikely(defer_queue.last_fct_in != fct
+			|| DQ_IS_FCT_BIT(p)
+			|| p == DQ_FCT_MARK)) {
 		defer_queue.last_fct_in = fct;
 		if (unlikely(DQ_IS_FCT_BIT(fct) || fct == DQ_FCT_MARK)) {
-			/*
-			 * If the function to encode is not aligned or the
-			 * marker, write DQ_FCT_MARK followed by the function
-			 * pointer.
-			 */
 			_CMM_STORE_SHARED(defer_queue.q[head++ & DEFER_QUEUE_MASK],
 				      DQ_FCT_MARK);
 			_CMM_STORE_SHARED(defer_queue.q[head++ & DEFER_QUEUE_MASK],
 				      fct);
 		} else {
 			DQ_SET_FCT_BIT(fct);
-			_CMM_STORE_SHARED(defer_queue.q[head++ & DEFER_QUEUE_MASK],
-				      fct);
-		}
-	} else {
-		if (unlikely(DQ_IS_FCT_BIT(p) || p == DQ_FCT_MARK)) {
-			/*
-			 * If the data to encode is not aligned or the marker,
-			 * write DQ_FCT_MARK followed by the function pointer.
-			 */
-			_CMM_STORE_SHARED(defer_queue.q[head++ & DEFER_QUEUE_MASK],
-				      DQ_FCT_MARK);
 			_CMM_STORE_SHARED(defer_queue.q[head++ & DEFER_QUEUE_MASK],
 				      fct);
 		}
