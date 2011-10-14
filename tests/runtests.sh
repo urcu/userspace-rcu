f8cbb9da4a7001f598f1c38f85712ab39fe0d938
Lai Jiangshan <laijs@cn.fujitsu.com>
2011-10-14 08:46:30 -0500

    rename test_qsbr to test_urcu_qsbr
    [ edit by Mathieu Desnoyers: update runtests*.sh scripts accordingly ]
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for a in test_urcu_gc test_urcu_signal_gc test_urcu_mb_gc test_qsbr_gc \
-	test_urcu_lgc test_urcu_signal_lgc test_urcu_mb_lgc test_qsbr_lgc \
-	test_urcu test_urcu_signal test_urcu_mb test_qsbr \
+for a in test_urcu_gc test_urcu_signal_gc test_urcu_mb_gc test_urcu_qsbr_gc \
+	test_urcu_lgc test_urcu_signal_lgc test_urcu_mb_lgc test_urcu_qsbr_lgc \
+	test_urcu test_urcu_signal test_urcu_mb test_urcu_qsbr \
 	test_rwlock test_perthreadlock test_mutex; do
 	echo "./${a} $*" | tee -a runall.detail.log
 	/usr/bin/time --append --output runall.detail.log ./${a} $*
