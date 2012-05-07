urcu/userspace-rcu

commit db00ccc36e7fb04ce8044fb1be7964acd1de6ae0

rculfhash: Relax atomicity guarantees required by removal operation
The atomicity guarantees for the removal operation do not need to be as
strict as a cmpxchg. Use a uatomic_set followed by a xchg on a newly
introduced "REMOVAL_OWNER_FLAG" to perform removal.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

rculfhash.c
@@ -201,7 +201,8 @@
  */
 #define REMOVED_FLAG		(1UL << 0)
 #define BUCKET_FLAG		(1UL << 1)
-#define FLAGS_MASK		((1UL << 2) - 1)
+#define REMOVAL_OWNER_FLAG	(1UL << 2)
+#define FLAGS_MASK		((1UL << 3) - 1)
 
 /* Value of the end pointer. Should not interact with flags. */
 #define END_VALUE		NULL
@@ -639,6 +640,18 @@ struct cds_lfht_node *flag_bucket(struct cds_lfht_node *node)
 }
 
 static
+int is_removal_owner(struct cds_lfht_node *node)
+{
+	return ((unsigned long) node) & REMOVAL_OWNER_FLAG;
+}
+
+static
+struct cds_lfht_node *flag_removal_owner(struct cds_lfht_node *node)
+{
+	return (struct cds_lfht_node *) (((unsigned long) node) | REMOVAL_OWNER_FLAG);
+}
+
+static
 struct cds_lfht_node *get_end(void)
 {
 	return (struct cds_lfht_node *) END_VALUE;
@@ -778,6 +791,9 @@ int _cds_lfht_replace(struct cds_lfht *ht, unsigned long size,
 		 * next pointer, they will either skip the old node due
 		 * to the removal flag and see the new node, or use
 		 * the old node, but will not see the new one.
+		 * This is a replacement of a node with another node
+		 * that has the same value: we are therefore not
+		 * removing a value from the hash table.
 		 */
 		ret_next = uatomic_cmpxchg(&old_node->next,
 			      old_next, flag_removed(new_node));
@@ -916,7 +932,7 @@ int _cds_lfht_del(struct cds_lfht *ht, unsigned long size,
 		struct cds_lfht_node *node,
 		int bucket_removal)
 {
-	struct cds_lfht_node *bucket, *next, *old;
+	struct cds_lfht_node *bucket, *next;
 
 	if (!node)	/* Return -ENOENT if asked to delete NULL node */
 		return -ENOENT;
@@ -924,20 +940,28 @@ int _cds_lfht_del(struct cds_lfht *ht, unsigned long size,
 	/* logically delete the node */
 	assert(!is_bucket(node));
 	assert(!is_removed(node));
-	old = rcu_dereference(node->next);
-	do {
-		struct cds_lfht_node *new_next;
+	assert(!is_removal_owner(node));
 
-		next = old;
-		if (caa_unlikely(is_removed(next)))
-			return -ENOENT;
-		if (bucket_removal)
-			assert(is_bucket(next));
-		else
-			assert(!is_bucket(next));
-		new_next = flag_removed(next);
-		old = uatomic_cmpxchg(&node->next, next, new_next);
-	} while (old != next);
+	/*
+	 * We are first checking if the node had previously been
+	 * logically removed (this check is not atomic with setting the
+	 * logical removal flag). Return -ENOENT if the node had
+	 * previously been removed.
+	 */
+	next = rcu_dereference(node->next);
+	if (caa_unlikely(is_removed(next)))
+		return -ENOENT;
+	if (bucket_removal)
+		assert(is_bucket(next));
+	else
+		assert(!is_bucket(next));
+	/*
+	 * We set the REMOVED_FLAG unconditionally. Note that there may
+	 * be more than one concurrent thread setting this flag.
+	 * Knowing which wins the race will be known after the garbage
+	 * collection phase, stay tuned!
+	 */
+	uatomic_or(&node->next, REMOVED_FLAG);
 	/* We performed the (logical) deletion. */
 
 	/*
@@ -949,7 +973,23 @@ int _cds_lfht_del(struct cds_lfht *ht, unsigned long size,
 	_cds_lfht_gc_bucket(bucket, node);
 
 	assert(is_removed(rcu_dereference(node->next)));
-	return 0;
+	/*
+	 * Last phase: atomically exchange node->next with a version
+	 * having "REMOVAL_OWNER_FLAG" set. If the returned node->next
+	 * pointer did _not_ have "REMOVAL_OWNER_FLAG" set, we now own
+	 * the node and win the removal race.
+	 * It is interesting to note that all "add" paths are forbidden
+	 * to change the next pointer starting from the point where the
+	 * REMOVED_FLAG is set, so here using a read, followed by a
+	 * xchg() suffice to guarantee that the xchg() will ever only
+	 * set the "REMOVAL_OWNER_FLAG" (or change nothing if the flag
+	 * was already set).
+	 */
+	if (!is_removal_owner(uatomic_xchg(&node->next,
+			flag_removal_owner(node->next))))
+		return 0;
+	else
+		return -ENOENT;
 }
 
 static

urcu/rculfhash.h
@@ -39,8 +39,8 @@ extern "C" {
  * cds_lfht_node: Contains the next pointers and reverse-hash
  * value required for lookup and traversal of the hash table.
  *
- * struct cds_lfht_node should be aligned on 4-bytes boundaries because
- * the two lower bits are used as flags.
+ * struct cds_lfht_node should be aligned on 8-bytes boundaries because
+ * the three lower bits are used as flags.
  *
  * struct cds_lfht_node can be embedded into a structure (as a field).
  * caa_container_of() can be used to get the structure from the struct
@@ -53,7 +53,7 @@ extern "C" {
 struct cds_lfht_node {
 	struct cds_lfht_node *next;	/* ptr | BUCKET_FLAG | REMOVED_FLAG */
 	unsigned long reverse_hash;
-} __attribute__((aligned(4)));
+} __attribute__((aligned(8)));
 
 /* cds_lfht_iter: Used to track state while traversing a hash chain. */
 struct cds_lfht_iter {

commit 71bb3aca70894bd78ec75f47c0c464261ad1dc86

rculfhash: fix: race between replace and del operations
Bug introduced by commit db00ccc36e7fb04ce8044fb1be7964acd1de6ae0

Here is the race:

Initially in hash table:  A

T0                                         T1
replace A by B
                                           del A
  read A->next
  -> check REMOVED flag, not set yet.
                                           read A->next
                                           -> check REMOVED flag, not set yet.
  cmpxchg A->next to set REMOVED flag
  -> cmpxchg succeeds
                                           uatomic_or to set REMOVED flag
                                           uatomic_xchg to atomically set the REMOVAL_OWNER flag
                                           -> first to set the flag.
  Replace returns node -> free(A)          Del success -> free(A)

With this race, we have a double-free.

The problem with the replace code is that it does not set the
"REMOVAL_OWNER" flag.

Test case to reproduce the bug:

test_urcu_hash 0 2 20 -A -s -M 1 -N 1 -O 1

(2 threads, doing replace/del, with a hash table that has only a single
key for all values). After just a couple of seconds, either the program
hangs, or, more often, it does:

*** glibc detected ***
/media/truecrypt1/compudj/doc/userspace-rcu/tests/.libs/test_urcu_hash:
malloc(): memory corruption (fast): 0x00007ffff3a29e25 ***

Program received signal SIGSEGV, Segmentation fault.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Tested-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

rculfhash.c
@@ -735,12 +735,6 @@ int is_removed(struct cds_lfht_node *node)
 }
 
 static
-struct cds_lfht_node *flag_removed(struct cds_lfht_node *node)
-{
-	return (struct cds_lfht_node *) (((unsigned long) node) | REMOVED_FLAG);
-}
-
-static
 int is_bucket(struct cds_lfht_node *node)
 {
 	return ((unsigned long) node) & BUCKET_FLAG;
@@ -762,6 +756,12 @@ static
 struct cds_lfht_node *flag_removal_owner(struct cds_lfht_node *node)
 {
 	return (struct cds_lfht_node *) (((unsigned long) node) | REMOVAL_OWNER_FLAG);
+}
+
+static
+struct cds_lfht_node *flag_removed_or_removal_owner(struct cds_lfht_node *node)
+{
+	return (struct cds_lfht_node *) (((unsigned long) node) | REMOVED_FLAG | REMOVAL_OWNER_FLAG);
 }
 
 static
@@ -894,6 +894,12 @@ int _cds_lfht_replace(struct cds_lfht *ht, unsigned long size,
 		}
 		assert(old_next == clear_flag(old_next));
 		assert(new_node != old_next);
+		/*
+		 * REMOVAL_OWNER flag is _NEVER_ set before the REMOVED
+		 * flag. It is either set atomically at the same time
+		 * (replace) or after (del).
+		 */
+		assert(!is_removal_owner(old_next));
 		new_node->next = old_next;
 		/*
 		 * Here is the whole trick for lock-free replace: we add
@@ -906,10 +912,12 @@ int _cds_lfht_replace(struct cds_lfht *ht, unsigned long size,
 		 * the old node, but will not see the new one.
 		 * This is a replacement of a node with another node
 		 * that has the same value: we are therefore not
-		 * removing a value from the hash table.
+		 * removing a value from the hash table. We set both the
+		 * REMOVED and REMOVAL_OWNER flags atomically so we own
+		 * the node after successful cmpxchg.
 		 */
 		ret_next = uatomic_cmpxchg(&old_node->next,
-			      old_next, flag_removed(new_node));
+			old_next, flag_removed_or_removal_owner(new_node));
 		if (ret_next == old_next)
 			break;		/* We performed the replacement. */
 		old_next = ret_next;