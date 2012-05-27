383dac33797fd0858d6858527324a6a15aa06c09
Hirohisa Yamaguchi <umq@ueo.co.jp>
2012-05-27 14:16:59 -0400

    tests: support FreeBSD short "time" args
    time(1) in FreeBSD does not have long argument name: change --append to
    -a and --output to -o
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/runtests-batch.sh
+++ tests/runtests-batch.sh
@@ -3,6 +3,6 @@
 #for a in test_urcu_gc test_urcu_gc_mb test_urcu_qsbr_gc; do
 for a in test_urcu_gc; do
 	echo "./${a} $*" | tee -a runall.detail.log
-	/usr/bin/time --append --output runall.detail.log ./${a} $*
+	/usr/bin/time -a -o runall.detail.log ./${a} $*
 done
 
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,6 +5,6 @@ for a in test_urcu_gc test_urcu_signal_gc test_urcu_mb_gc test_urcu_qsbr_gc \
 	test_urcu test_urcu_signal test_urcu_mb test_urcu_qsbr \
 	test_rwlock test_perthreadlock test_mutex; do
 	echo "./${a} $*" | tee -a runall.detail.log
-	/usr/bin/time --append --output runall.detail.log ./${a} $*
+	/usr/bin/time -a -o runall.detail.log ./${a} $*
 done
 
