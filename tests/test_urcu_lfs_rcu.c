cb8818f01ed341f21d9b6467365a66fda3fc6856
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-10-11 16:44:40 -0400

    lfstack: implement test
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/test_urcu_lfs_rcu.c
+++ tests/test_urcu_lfs_rcu.c
@@ -1,9 +1,9 @@
 /*
- * test_urcu_lfs.c
+ * test_urcu_lfs_rcu.c
  *
- * Userspace RCU library - example lock-free stack
+ * Userspace RCU library - example RCU-based lock-free stack
  *
- * Copyright 2010-2012 - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ * Copyright February 2010 - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
  * Copyright February 2010 - Paolo Bonzini <pbonzini@redhat.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -105,10 +105,9 @@ typedef unsigned long cpu_set_t;
 
 static void set_affinity(void)
 {
-#if HAVE_SCHED_SETAFFINITY
 	cpu_set_t mask;
-	int cpu, ret;
-#endif /* HAVE_SCHED_SETAFFINITY */
+	int cpu;
+	int ret;
 
 	if (!use_affinity)
 		return;
@@ -159,11 +158,11 @@ static unsigned int nr_enqueuers;
 static unsigned int nr_dequeuers;
 
 struct test {
-	struct cds_lfs_node list;
+	struct cds_lfs_node_rcu list;
 	struct rcu_head rcu;
 };
 
-static struct cds_lfs_stack s;
+static struct cds_lfs_stack_rcu s;
 
 void *thr_enqueuer(void *_count)
 {
@@ -185,8 +184,9 @@ void *thr_enqueuer(void *_count)
 		struct test *node = malloc(sizeof(*node));
 		if (!node)
 			goto fail;
-		cds_lfs_node_init(&node->list);
-		cds_lfs_push(&s, &node->list);
+		cds_lfs_node_init_rcu(&node->list);
+		/* No rcu read-side is needed for push */
+		cds_lfs_push_rcu(&s, &node->list);
 		URCU_TLS(nr_successful_enqueues)++;
 
 		if (caa_unlikely(wdelay))
@@ -234,10 +234,10 @@ void *thr_dequeuer(void *_count)
 	cmm_smp_mb();
 
 	for (;;) {
-		struct cds_lfs_node *snode;
+		struct cds_lfs_node_rcu *snode;
 
 		rcu_read_lock();
-		snode = cds_lfs_pop(&s);
+		snode = cds_lfs_pop_rcu(&s);
 		rcu_read_unlock();
 		if (snode) {
 			struct test *node;
@@ -264,12 +264,12 @@ void *thr_dequeuer(void *_count)
 	return ((void*)2);
 }
 
-void test_end(struct cds_lfs_stack *s, unsigned long long *nr_dequeues)
+void test_end(struct cds_lfs_stack_rcu *s, unsigned long long *nr_dequeues)
 {
-	struct cds_lfs_node *snode;
+	struct cds_lfs_node_rcu *snode;
 
 	do {
-		snode = cds_lfs_pop(s);
+		snode = cds_lfs_pop_rcu(s);
 		if (snode) {
 			struct test *node;
 
@@ -371,7 +371,7 @@ int main(int argc, char **argv)
 	tid_dequeuer = malloc(sizeof(*tid_dequeuer) * nr_dequeuers);
 	count_enqueuer = malloc(2 * sizeof(*count_enqueuer) * nr_enqueuers);
 	count_dequeuer = malloc(2 * sizeof(*count_dequeuer) * nr_dequeuers);
-	cds_lfs_init(&s);
+	cds_lfs_init_rcu(&s);
 	err = create_all_cpu_call_rcu_data(0);
 	if (err) {
 		printf("Per-CPU call_rcu() worker threads unavailable. Using default global worker thread.\n");
