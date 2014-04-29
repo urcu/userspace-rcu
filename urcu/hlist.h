urcu/userspace-rcu

commit db903109f0031c831e8fdc95cb7197996e53f46d

Fix undefined NULL pointer arithmetic in hlist
Clang 3.3 with -O2 optimisations is especially picky about arithmetic on
NULL pointers. This undefined behavior is turned into optimized out NULL
checks by clang 3.3. Fix the undefined behavior by checking against the
pointer directly, without going back and forth around NULL with
pointer arithmetic.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

urcu/hlist.h
@@ -93,14 +93,18 @@ void cds_hlist_del(struct cds_hlist_node *elem)
 			entry = cds_hlist_entry(pos, __typeof__(*entry), member))
 
 #define cds_hlist_for_each_entry_2(entry, head, member) \
-	for (entry = cds_hlist_entry((head)->next, __typeof__(*entry), member); \
-		&entry->member != NULL; \
-		entry = cds_hlist_entry(entry->member.next, __typeof__(*entry), member))
+	for (entry = ((head)->next == NULL ? NULL \
+			: cds_hlist_entry((head)->next, __typeof__(*entry), member)); \
+		entry != NULL; \
+		entry = (entry->member.next == NULL ? NULL \
+			: cds_hlist_entry(entry->member.next, __typeof__(*entry), member)))
 
 #define cds_hlist_for_each_entry_safe_2(entry, e, head, member) \
-	for (entry = cds_hlist_entry((head)->next, __typeof__(*entry), member); \
-		(&entry->member != NULL) && (e = cds_hlist_entry(entry->member.next, \
-						__typeof__(*entry), member), 1); \
+	for (entry = ((head)->next == NULL ? NULL \
+			: cds_hlist_entry((head)->next, __typeof__(*entry), member)); \
+		(entry != NULL) && (e = (entry->member.next == NULL ? NULL \
+					: cds_hlist_entry(entry->member.next, \
+						__typeof__(*entry), member), 1)); \
 		entry = e)
 
 #endif	/* _KCOMPAT_HLIST_H */

commit 79efd9b350e07a499b3620ddde4860b8378fcf00

Fix: incorrect parenthesis in cds_hlist_for_each_entry_safe_2
commit db903109f0031c831e8fdc95cb7197996e53f46d introduced a regression
in cds_hlist_for_each_entry_safe_2(): incorrect parenthesis assign "e"
to 1, rather than assign "e" to the next pointer, and evaluating the
expression to 1 (comma expression).

Reported-by: Daniel Thibault <Daniel.Thibault@drdc-rddc.gc.ca>
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

urcu/hlist.h
@@ -104,7 +104,7 @@ void cds_hlist_del(struct cds_hlist_node *elem)
 			: cds_hlist_entry((head)->next, __typeof__(*entry), member)); \
 		(entry != NULL) && (e = (entry->member.next == NULL ? NULL \
 					: cds_hlist_entry(entry->member.next, \
-						__typeof__(*entry), member), 1)); \
+						__typeof__(*entry), member)), 1); \
 		entry = e)
 
 #endif	/* _KCOMPAT_HLIST_H */