8d11f7cd74968f5baffdbfde8b38d3197ef6865d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-03-15 21:15:41 -0400

    tests: Use stderr redirection for time output
    Allows using Mac OS X time command in tests.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/benchmark/runtests.sh
+++ tests/benchmark/runtests.sh
@@ -6,7 +6,7 @@ log_file="runall.detail.log"
 
 # Check if time bin is non-empty
 if [ -n "$test_time_bin" ]; then
-	time_command="$test_time_bin -a -o $log_file"
+	time_command="$test_time_bin"
 else
 	time_command=""
 fi
@@ -16,6 +16,6 @@ for a in test_urcu_gc test_urcu_signal_gc test_urcu_mb_gc test_urcu_qsbr_gc \
 	test_urcu test_urcu_signal test_urcu_mb test_urcu_qsbr \
 	test_rwlock test_perthreadlock test_mutex; do
 	echo "./${a} $*" | tee -a "$log_file"
-	$time_command ./${a} $*
+	$time_command ./${a} $* 2>> $log_file
 done
 
