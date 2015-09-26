33f0d0b1c3c9ee55e49b6f7bed146e483e3fd935
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-09-26 11:17:18 -0400

    s390: allocate membarrier system call number
    Now that the membarrier system call is allocated on s390/s390x, allocate
    its number in our architecture header if the system headers don't
    allocate it. This allows using the membarrier system call as soon as
    implemented in the kernel, even if the distribution has old kernel
    headers.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- urcu/arch/s390.h
+++ urcu/arch/s390.h
@@ -51,6 +51,14 @@ static inline cycles_t caa_get_cycles (void)
 	return cycles;
 }
 
+/*
+ * Define the membarrier system call number if not yet available in the
+ * system headers.
+ */
+#ifndef __NR_membarrier
+#define __NR_membarrier		356
+#endif
+
 #ifdef __cplusplus 
 }
 #endif
