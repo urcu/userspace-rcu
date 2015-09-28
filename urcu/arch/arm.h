da5b715a201a36156fa520b6442db7c46f6fdade
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-09-28 10:46:54 -0400

    arm: allocate membarrier system call number
    Now that the membarrier system call is allocated on ARM, allocate its
    number in our architecture header if the system headers don't allocate
    it. This allows using the membarrier system call as soon as implemented
    in the kernel, even if the distribution has old kernel headers.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- urcu/arch/arm.h
+++ urcu/arch/arm.h
@@ -52,6 +52,14 @@ static inline cycles_t caa_get_cycles (void)
 	return (cycles_t)thetime;
 }
 
+/*
+ * Define the membarrier system call number if not yet available in the
+ * system headers.
+ */
+#ifndef __NR_membarrier
+#define __NR_membarrier		389
+#endif
+
 #ifdef __cplusplus 
 }
 #endif
