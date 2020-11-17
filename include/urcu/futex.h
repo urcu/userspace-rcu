ca91060ec7b0c64249c514ee4114753443ecd70f
Michael Jeanson <mjeanson@efficios.com>
2020-11-17 18:28:20 -0500

    Compile time futex syscall detection
    The futex syscall availability is detected at configure time using a
    simple compile test. Replace this with compile time detection using the
    same test. This would allow sharing headers in a multi-arch environment
    and reduce the build system complexity.
    
    Change-Id: Ief709429e8908d0fd2d268d42f4a89d9fc14b91b
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/urcu/futex.h
+++ include/urcu/futex.h
@@ -24,6 +24,8 @@
  */
 
 #include <urcu/config.h>
+#include <urcu/syscall-compat.h>
+
 #include <errno.h>
 #include <stdint.h>
 #include <time.h>
@@ -54,7 +56,10 @@ extern int compat_futex_noasync(int32_t *uaddr, int op, int32_t val,
 extern int compat_futex_async(int32_t *uaddr, int op, int32_t val,
 		const struct timespec *timeout, int32_t *uaddr2, int32_t val3);
 
-#ifdef CONFIG_RCU_HAVE_FUTEX
+#if (defined(__linux__) && defined(__NR_futex_))
+
+/* For backwards compat */
+#define CONFIG_RCU_HAVE_FUTEX 1
 
 #include <unistd.h>
 #include <errno.h>
