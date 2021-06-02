c0ecb6ea53e564c5bca028070b43793afb77a790
Michael Jeanson <mjeanson@efficios.com>
2021-06-02 10:55:22 -0400

    Add serialized ABI definition files
    This commit contains the serialized ABI definitions for a typical build
    of the liburcu librairies. This information is extracted using
    libabigail (https://sourceware.org/libabigail/).
    
    The artefacts used to generate these were built with CFLAGS="-O0 -ggdb".
    
    You can compare the serialized ABI with a shared object to check for
    breaking changes. For example, here we compare an in-tree built version
    of liburcu-memb.so with the serialized ABI of stable-0.13 :
    
      abidiff \
        extras/abi/0.13/x86_64-pc-linux-gnu/liburcu-memb.so.7.xml \
        src/.libs/liburcu-memb.so
    
    Change-Id: Icb3145cdf9f69490981ce201ec437cc83298f0a7
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ extras/abi/dump_abi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-only
+
+set -eu
+
+INDIR=$1
+OUTDIR=$2
+
+ARGS=(
+	"--annotate" # Add comments to the xml output
+	"--no-corpus-path" # Do not put the path in the abi-corpus
+)
+
+for lib in "${INDIR}"/liburcu*.so.?
+do
+	abidw "${ARGS[@]}" --out-file "${OUTDIR}/$(basename "$lib").xml" "$lib"
+
+	# Clean the full paths
+	sed -i "s#$(pwd)/##g" "${OUTDIR}/$(basename "$lib").xml"
+done
+
