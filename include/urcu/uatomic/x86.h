835b9ab3ca3777fe42e37e92096226ebd19ca75b
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2021-06-15 15:28:24 -0400

    Fix: x86 and s390 uatomic: __hp() macro warning with gcc 11
    The __hp() macro used in the x86 and s390 uatomic code generates the
    following warning with gcc-11:
    
    In file included from ../include/urcu/uatomic.h:27,
                     from ../include/urcu/static/wfcqueue.h:35,
                     from ../include/urcu/wfcqueue.h:133,
                     from workqueue.c:39:
    workqueue.c: In function ‘workqueue_thread’:
    ../include/urcu/uatomic/x86.h:155:17: warning: array subscript ‘struct __uatomic_dummy[0]’ is partly outside array bounds of ‘struct cds_wfcq_tail[1]’ [-Warray-bounds]
      155 |                 __asm__ __volatile__(
          |                 ^~~~~~~
    workqueue.c:184:38: note: while referencing ‘cbs_tmp_tail’
      184 |                 struct cds_wfcq_tail cbs_tmp_tail;
          |                                      ^~~~~~~~~~~~
    
    The (previously undocumented) reason for this macro is to allow passing the
    "void *" parameter as "m" or "+m" operand to the inline assembly. That
    motivation was explained in commit 53b8ed6836363 ("s390 uatomic arch fix").
    
    The out of bound access is detected by gcc because struct
    __uatomic_dummy's length is quite large: an array of 10 unsigned long,
    which is larger than the size pointed to by the void pointer.
    
    So rather than using a fixed-size type, cast to a structure containing
    an array of characters of a size matching the @addr input argument.
    
    While we are at it and digging out git archeology, properly document the
    __hp() macro for posterity.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I785e30661efe48be1806664a1a14fd3c9fdb0a32

--- include/urcu/uatomic/x86.h
+++ include/urcu/uatomic/x86.h
@@ -36,10 +36,14 @@ extern "C" {
  * Derived from AO_compare_and_swap() and AO_test_and_set_full().
  */
 
-struct __uatomic_dummy {
-	unsigned long v[10];
-};
-#define __hp(x)	((struct __uatomic_dummy *)(x))
+/*
+ * The __hp() macro casts the void pointer "x" to a pointer to a structure
+ * containing an array of char of the specified size. This allows passing the
+ * @addr arguments of the following inline functions as "m" and "+m" operands
+ * to the assembly.
+ */
+
+#define __hp(size, x)	((struct { char v[size]; } *)(x))
 
 #define _uatomic_set(addr, v)	((void) CMM_STORE_SHARED(*(addr), (v)))
 
@@ -56,7 +60,7 @@ unsigned long __uatomic_cmpxchg(void *addr, unsigned long old,
 
 		__asm__ __volatile__(
 		"lock; cmpxchgb %2, %1"
-			: "+a"(result), "+m"(*__hp(addr))
+			: "+a"(result), "+m"(*__hp(len, addr))
 			: "q"((unsigned char)_new)
 			: "memory");
 		return result;
@@ -67,7 +71,7 @@ unsigned long __uatomic_cmpxchg(void *addr, unsigned long old,
 
 		__asm__ __volatile__(
 		"lock; cmpxchgw %2, %1"
-			: "+a"(result), "+m"(*__hp(addr))
+			: "+a"(result), "+m"(*__hp(len, addr))
 			: "r"((unsigned short)_new)
 			: "memory");
 		return result;
@@ -78,7 +82,7 @@ unsigned long __uatomic_cmpxchg(void *addr, unsigned long old,
 
 		__asm__ __volatile__(
 		"lock; cmpxchgl %2, %1"
-			: "+a"(result), "+m"(*__hp(addr))
+			: "+a"(result), "+m"(*__hp(len, addr))
 			: "r"((unsigned int)_new)
 			: "memory");
 		return result;
@@ -90,7 +94,7 @@ unsigned long __uatomic_cmpxchg(void *addr, unsigned long old,
 
 		__asm__ __volatile__(
 		"lock; cmpxchgq %2, %1"
-			: "+a"(result), "+m"(*__hp(addr))
+			: "+a"(result), "+m"(*__hp(len, addr))
 			: "r"((unsigned long)_new)
 			: "memory");
 		return result;
@@ -123,7 +127,7 @@ unsigned long __uatomic_exchange(void *addr, unsigned long val, int len)
 		unsigned char result;
 		__asm__ __volatile__(
 		"xchgb %0, %1"
-			: "=q"(result), "+m"(*__hp(addr))
+			: "=q"(result), "+m"(*__hp(len, addr))
 			: "0" ((unsigned char)val)
 			: "memory");
 		return result;
@@ -133,7 +137,7 @@ unsigned long __uatomic_exchange(void *addr, unsigned long val, int len)
 		unsigned short result;
 		__asm__ __volatile__(
 		"xchgw %0, %1"
-			: "=r"(result), "+m"(*__hp(addr))
+			: "=r"(result), "+m"(*__hp(len, addr))
 			: "0" ((unsigned short)val)
 			: "memory");
 		return result;
@@ -143,7 +147,7 @@ unsigned long __uatomic_exchange(void *addr, unsigned long val, int len)
 		unsigned int result;
 		__asm__ __volatile__(
 		"xchgl %0, %1"
-			: "=r"(result), "+m"(*__hp(addr))
+			: "=r"(result), "+m"(*__hp(len, addr))
 			: "0" ((unsigned int)val)
 			: "memory");
 		return result;
@@ -154,7 +158,7 @@ unsigned long __uatomic_exchange(void *addr, unsigned long val, int len)
 		unsigned long result;
 		__asm__ __volatile__(
 		"xchgq %0, %1"
-			: "=r"(result), "+m"(*__hp(addr))
+			: "=r"(result), "+m"(*__hp(len, addr))
 			: "0" ((unsigned long)val)
 			: "memory");
 		return result;
@@ -187,7 +191,7 @@ unsigned long __uatomic_add_return(void *addr, unsigned long val,
 
 		__asm__ __volatile__(
 		"lock; xaddb %1, %0"
-			: "+m"(*__hp(addr)), "+q" (result)
+			: "+m"(*__hp(len, addr)), "+q" (result)
 			:
 			: "memory");
 		return result + (unsigned char)val;
@@ -198,7 +202,7 @@ unsigned long __uatomic_add_return(void *addr, unsigned long val,
 
 		__asm__ __volatile__(
 		"lock; xaddw %1, %0"
-			: "+m"(*__hp(addr)), "+r" (result)
+			: "+m"(*__hp(len, addr)), "+r" (result)
 			:
 			: "memory");
 		return result + (unsigned short)val;
@@ -209,7 +213,7 @@ unsigned long __uatomic_add_return(void *addr, unsigned long val,
 
 		__asm__ __volatile__(
 		"lock; xaddl %1, %0"
-			: "+m"(*__hp(addr)), "+r" (result)
+			: "+m"(*__hp(len, addr)), "+r" (result)
 			:
 			: "memory");
 		return result + (unsigned int)val;
@@ -221,7 +225,7 @@ unsigned long __uatomic_add_return(void *addr, unsigned long val,
 
 		__asm__ __volatile__(
 		"lock; xaddq %1, %0"
-			: "+m"(*__hp(addr)), "+r" (result)
+			: "+m"(*__hp(len, addr)), "+r" (result)
 			:
 			: "memory");
 		return result + (unsigned long)val;
@@ -251,7 +255,7 @@ void __uatomic_and(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; andb %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "iq" ((unsigned char)val)
 			: "memory");
 		return;
@@ -260,7 +264,7 @@ void __uatomic_and(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; andw %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "ir" ((unsigned short)val)
 			: "memory");
 		return;
@@ -269,7 +273,7 @@ void __uatomic_and(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; andl %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "ir" ((unsigned int)val)
 			: "memory");
 		return;
@@ -279,7 +283,7 @@ void __uatomic_and(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; andq %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "er" ((unsigned long)val)
 			: "memory");
 		return;
@@ -307,7 +311,7 @@ void __uatomic_or(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; orb %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "iq" ((unsigned char)val)
 			: "memory");
 		return;
@@ -316,7 +320,7 @@ void __uatomic_or(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; orw %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "ir" ((unsigned short)val)
 			: "memory");
 		return;
@@ -325,7 +329,7 @@ void __uatomic_or(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; orl %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "ir" ((unsigned int)val)
 			: "memory");
 		return;
@@ -335,7 +339,7 @@ void __uatomic_or(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; orq %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "er" ((unsigned long)val)
 			: "memory");
 		return;
@@ -363,7 +367,7 @@ void __uatomic_add(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; addb %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "iq" ((unsigned char)val)
 			: "memory");
 		return;
@@ -372,7 +376,7 @@ void __uatomic_add(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; addw %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "ir" ((unsigned short)val)
 			: "memory");
 		return;
@@ -381,7 +385,7 @@ void __uatomic_add(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; addl %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "ir" ((unsigned int)val)
 			: "memory");
 		return;
@@ -391,7 +395,7 @@ void __uatomic_add(void *addr, unsigned long val, int len)
 	{
 		__asm__ __volatile__(
 		"lock; addq %1, %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			: "er" ((unsigned long)val)
 			: "memory");
 		return;
@@ -420,7 +424,7 @@ void __uatomic_inc(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; incb %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -429,7 +433,7 @@ void __uatomic_inc(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; incw %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -438,7 +442,7 @@ void __uatomic_inc(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; incl %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -448,7 +452,7 @@ void __uatomic_inc(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; incq %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -473,7 +477,7 @@ void __uatomic_dec(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; decb %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -482,7 +486,7 @@ void __uatomic_dec(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; decw %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -491,7 +495,7 @@ void __uatomic_dec(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; decl %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
@@ -501,7 +505,7 @@ void __uatomic_dec(void *addr, int len)
 	{
 		__asm__ __volatile__(
 		"lock; decq %0"
-			: "=m"(*__hp(addr))
+			: "=m"(*__hp(len, addr))
 			:
 			: "memory");
 		return;
