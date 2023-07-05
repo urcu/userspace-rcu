c5e71b137f1ea8e92b41315b8f5c9be5f38bf699
Michael Jeanson <mjeanson@efficios.com>
2023-07-05 14:20:10 -0400

    extras/abi: license data files under CC-1.0
    The SPDX identifiers [1] are a legally binding shorthand, which can be
    used instead of the full boiler plate text. This is another step towards
    implementing the full REUSE spec [2] to help with copyright and
    licensing audits and compliance.
    
    This will reduce a lot a manual work required for the licensing audit
    required in Debian on each update.
    
    These are generated files, use the CC-1.0 license to make their
    licensing clear.
    
    [1] https://spdx.org/ids-how
    [2] https://reuse.software/tutorial/
    
    Change-Id: I2ac79f2646ced6c01bce67c1281a2d597ab0d890
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- extras/abi/dump_abi.sh
+++ extras/abi/dump_abi.sh
@@ -14,11 +14,20 @@ ARGS=(
 	"--no-corpus-path" # Do not put the path in the abi-corpus
 )
 
+# Older version of the reuse tool are a bit dumb, split the tags string to make
+# it happy.
+spdx="SPDX"
+copyright="FileCopyrightText"
+license="License-Identifier"
+
 for lib in "${INDIR}"/liburcu*.so.?
 do
 	abidw "${ARGS[@]}" --out-file "${OUTDIR}/$(basename "$lib").xml" "$lib"
 
 	# Clean the full paths
 	sed -i "s#$(pwd)/##g" "${OUTDIR}/$(basename "$lib").xml"
+
+	# Add SPDX headers
+	sed -i "2 i <!--\n${spdx}-${copyright}: $(date +%Y) EfficiOS Inc.\n\n${spdx}-${license}: CC0-1.0\n-->" "${OUTDIR}/$(basename "$lib").xml"
 done
 
