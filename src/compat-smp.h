2e44aea918890114b82b3e0a66323192db89e375
Benjamin Marzinski via lttng-dev <lttng-dev@lists.lttng.org>
2024-05-01 19:42:41 -0400

    fix: handle EINTR correctly in get_cpu_mask_from_sysfs
    If the read() in get_cpu_mask_from_sysfs() fails with EINTR, the code is
    supposed to retry, but the while loop condition has (bytes_read > 0),
    which is false when read() fails with EINTR. The result is that the code
    exits the loop, having only read part of the string.
    
    Use (bytes_read != 0) in the while loop condition instead, since the
    (bytes_read < 0) case is already handled in the loop.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I565030d4625ae199cabc4c2ab5eb8ac49ea4dfcb

--- src/compat-smp.h
+++ src/compat-smp.h
@@ -164,7 +164,7 @@ static inline int get_cpu_mask_from_sysfs(char *buf, size_t max_bytes, const cha
 
 		total_bytes_read += bytes_read;
 		assert(total_bytes_read <= max_bytes);
-	} while (max_bytes > total_bytes_read && bytes_read > 0);
+	} while (max_bytes > total_bytes_read && bytes_read != 0);
 
 	/*
 	 * Make sure the mask read is a null terminated string.
