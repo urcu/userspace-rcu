353062d91df15e021c3dc10e92cbb4fe7ec23ce3
Christos Zoulas <christos@zoulas.com>
2025-01-16 16:37:38 -0500

    Signed-off-by: Christos Zoulas <christos@zoulas.com> Tested-by: Christos Zoulas <christos@zoulas.com>
    Add futex implementation for NetBSD
    
    regtest
    ============================================================================
    Testsuite summary for userspace-rcu 0.15.0
    ============================================================================
     TOTAL: 852
     PASS:  852
     SKIP:  0
     XFAIL: 0
     FAIL:  0
     XPASS: 0
     ERROR: 0
    ============================================================================
    TIME=23:04.34 CPU=1153.2% (15409.920u 554.945s) SWAPS=0 (3296+3746659)pf (0i+222o) (1Kc+30Kd)
    
    short_bench
    ============================================================================
    Testsuite summary for userspace-rcu 0.15.0
    ============================================================================
    ============================================================================
    TIME=45:40.15 CPU=1250.5% (33948.806u 318.658s) SWAPS=0 (953+3130345)pf (0i+285o) (0Kc+6Kd)

--- include/urcu/syscall-compat.h
+++ include/urcu/syscall-compat.h
@@ -13,10 +13,12 @@
 #include <sys/syscall.h>
 #elif defined(__linux__) || defined(__GLIBC__)
 #include <syscall.h>
+#elif defined(__NetBSD__)
+#include <sys/syscall.h>
 
 #elif defined(__CYGWIN__) || defined(__APPLE__) || \
 	defined(__FreeBSD__) || defined(__DragonFly__) || \
-	defined(__OpenBSD__) || defined(__NetBSD__)
+	defined(__OpenBSD__)
 /* Don't include anything on these platforms. */
 
 #else
