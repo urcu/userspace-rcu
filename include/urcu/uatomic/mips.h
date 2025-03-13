0316adc0f667125b4bc686caeee8c02d3f5c84d8
Michael Jeanson <mjeanson@efficios.com>
2025-03-13 15:15:05 -0400

    Add UATOMIC_HAS_ATOMIC_INT/LLONG for mips
    Change-Id: I59bffa23c6979a73904a240c704e399b41efe91b
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/urcu/uatomic/mips.h
+++ include/urcu/uatomic/mips.h
@@ -11,6 +11,22 @@
 
 #include <urcu/compiler.h>
 #include <urcu/system.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* #define UATOMIC_HAS_ATOMIC_BYTE */
+/* #define UATOMIC_HAS_ATOMIC_SHORT */
+#define UATOMIC_HAS_ATOMIC_INT
+#if (CAA_BITS_PER_LONG == 64)
+#define UATOMIC_HAS_ATOMIC_LLONG
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
 #include <urcu/uatomic/generic.h>
 
 #endif /* _URCU_UATOMIC_ARCH_MIPS_H */
