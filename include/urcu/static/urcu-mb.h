1fb48d36dc58e3c18a8e261177c087d357323d35
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2026-01-07 13:13:57 -0500

    urcu-mb: Use CMM_SEQ_CST_FENCE for _urcu_mb_read_lock_update
    _urcu_mb_read_lock_update currently does a relaxed store to
    rcu_reader.ctr followed by a cmm_smp_mb() (full barrier).
    
    The store and the full barrier can be combined into a CMM_SEQ_CST_FENCE
    store while preserving the same semantic. It allows architectures such
    as x86-64 to combine the store and the barrier into a more efficient
    instruction, e.g. lock-prefixed xchg.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I045a62f6fe75ea114440ea9a919c8d8e4bf48c09

--- include/urcu/static/urcu-mb.h
+++ include/urcu/static/urcu-mb.h
@@ -51,15 +51,15 @@ extern DECLARE_URCU_TLS(struct urcu_reader, urcu_mb_reader);
  * Helper for _urcu_mb_read_lock().  The format of urcu_mb_gp.ctr (as well as
  * the per-thread rcu_reader.ctr) has the lower-order bits containing a count of
  * _urcu_mb_read_lock() nesting, and a single high-order URCU_BP_GP_CTR_PHASE bit
- * that contains either zero or one.  The cmm_smp_mb() ensures that the accesses in
- * _urcu_mb_read_lock() happen before the subsequent read-side critical section.
+ * that contains either zero or one.  The CMM_SEQ_CST_FENCE store to
+ * rcu_reader.ctr ensures that the accesses in _urcu_mb_read_lock() happen before
+ * the subsequent read-side critical section.
  */
 static inline void _urcu_mb_read_lock_update(unsigned long tmp)
 {
-	if (caa_likely(!(tmp & URCU_GP_CTR_NEST_MASK))) {
-		uatomic_store(&URCU_TLS(urcu_mb_reader).ctr, uatomic_load(&urcu_mb_gp.ctr));
-		cmm_smp_mb();
-	} else
+	if (caa_likely(!(tmp & URCU_GP_CTR_NEST_MASK)))
+		uatomic_store(&URCU_TLS(urcu_mb_reader).ctr, uatomic_load(&urcu_mb_gp.ctr), CMM_SEQ_CST_FENCE);
+	else
 		uatomic_store(&URCU_TLS(urcu_mb_reader).ctr, tmp + URCU_GP_COUNT);
 }
 
